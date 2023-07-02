15625f3b0ec81e90275bf80f0aa1cf0ac01c6444
Harsh <imharshalashi333@gmail.com>
2023-07-02 10:50:50 +0530

    Home Page, Database connection using MongoDB done.

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,5 @@
+const CreatePrompt = (props) => {
+  return <div>Create Prompt</div>;
+};
+
+export default CreatePrompt;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,18 +7,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 import { set } from 'mongoose';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession(); // To check whether user is logged in or not.
 
-  const [providers, setProviders] = useState(null);
+  const [providers, setProviders] = useState(null); //For sign in purpose
 
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -33,9 +33,10 @@ const Nav = () => {
         />
         <p className="logo_text">Promtopia</p>
       </Link>
+
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -46,7 +47,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -73,10 +74,10 @@ const Nav = () => {
 
       {/* Mobile Navigation  */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
